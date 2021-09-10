907747cd1f68a091c27781ab351846bddde8e1f2
FoxyHart <88792549+FoxyHart@users.noreply.github.com>
2021-09-10 13:15:29 -0400

    Adding Pokemon to list
    I am adding 12 Pokemon and their corresponding types, heights and weights.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-alert('Hello world');
-let favoriteFood = 'Queso';
-document.write(favoriteFood);
+let pokemonlist = [ 
+    { name: 'Bulbasaur', type: [ 'Grass' , 'Poison'], height : 2, weight: 15 },
+    { name: 'Ivysaur', type: ['Grass' , 'Posion'], height: 3, weight: 29 },
+    { name: 'Venusaur', type: ['Grass' , 'Posion'], height: 6, weight: 220},
+    { name: 'Charmander', type: ['Fire'], height: 2, weight: 18},
+    { name: 'Charmeleon', type: ['Fire'], height: 3, weight: 42},
+    { name: 'Charizard', type: ['Fire' , 'Flying'], height: 5, weight: 200},
+    { name: 'Squirtle', type: ['Water'], height: 2, weight: 20},
+    { name: 'Warturtle', type: ['Water'], height: 3, weight: 50},
+    { name: 'Blastiose', type: ['Water'], height: 5, weight: 189},
+    { name: 'Caterpie', type: ['Bug'], height: 1, weight: 6},
+    { name: 'Metapod', type: ['Bug'], height: 2, weight:22},
+    { name: 'Butterfree', type: ['Bug' , 'Flying'], height: 3, weight: 70},
+];
+//for adding to pokedex entry {name: '', type: ['' , ''], height: , weight: }, or {name: '', type: [''], height: , weight: }, //
