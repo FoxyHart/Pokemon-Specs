ebca1ae9fcaaaf18aa7bb8fa9118645becd3b527
FoxyHart <88792549+FoxyHart@users.noreply.github.com>
2021-09-21 13:13:14 -0400

    Scripts.js update
    pushing update in an attempt to get the loop code to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,50 @@
-let pokemonlist = [ 
-    { name: 'Bulbasaur', type: [ 'Grass' , 'Poison'], height : 2, weight: 15 },
-    { name: 'Ivysaur', type: ['Grass' , 'Posion'], height: 3, weight: 29 },
-    { name: 'Venusaur', type: ['Grass' , 'Posion'], height: 6, weight: 220},
-    { name: 'Charmander', type: ['Fire'], height: 2, weight: 18},
-    { name: 'Charmeleon', type: ['Fire'], height: 3, weight: 42},
-    { name: 'Charizard', type: ['Fire' , 'Flying'], height: 5, weight: 200},
-    { name: 'Squirtle', type: ['Water'], height: 2, weight: 20},
-    { name: 'Warturtle', type: ['Water'], height: 3, weight: 50},
-    { name: 'Blastiose', type: ['Water'], height: 5, weight: 189},
-    { name: 'Caterpie', type: ['Bug'], height: 1, weight: 6},
-    { name: 'Metapod', type: ['Bug'], height: 2, weight:22},
-    { name: 'Butterfree', type: ['Bug' , 'Flying'], height: 3, weight: 70},
-];
-//for adding to pokedex entry {name: '', type: ['' , ''], height: , weight: }, or {name: '', type: [''], height: , weight: }, //
+let pokemonList = [ 
+    { name: "Chespin", type: [ 'Grass' ], height : "1", weight: "20" },
+    { name: "Quilladin", type: ['Grass' ], height: "2", weight: "64" },
+    { name: "Chesnaught", type: ['Grass' , 'Fighting'], height: "5", weight: "199"},
+    { name: "Fennekin", type: ['Fire'], height: "1", weight: "20"},
+    { name: "Braixen", type: ['Fire'], height: "3", weight: "32"},
+    { name: "Delphox", type: ['Fire' , 'Psychic'], height: "5", weight: "86"},
+    { name: "Froakie", type: ['Water'], height: "1", weight: "15"},
+    { name: "Frogadier", type: ['Water'], height: "2", weight: "24"},
+    { name: "Greninja", type: ['Water' , 'Dark'], height: "6", weight: "88"},
+    { name: "Bunnelby", type: ['Normal'], height: "1", weight: "11"},
+    { name: "Diggersby", type: ['Normal' , 'Ground'], height: "3", weight: "93"},
+    { name: "Zigzagoon", type: ['Normal'], height: "1", weight: "39"},
+    { name: "Linoone", type: ['Normal'], height: "1", weight: "72"}
+]
+//document.write ("Chespin-" + " Type: Grass," + " Height: 1," + " Weight: 20");
+//for adding to pokedex entry { name: '', type: ['' , ''], height: , weight: }, or { name: '', type: [''], height: , weight: }, 
+
+   // loop to write out pokemon type, height and weight, listing one pokemon as larger
+for (let i = 0; i < pokemonlist.length; i++) {
+    // varibles for pokemons names, height, weight and type
+    let pokemonName = pokemonList[i].name;
+    // let pokemonType = pokemonList[i].type;
+    let pokemonHeight = pokemonList[i].height;
+   // let pokemonWeight = pokemonList[i].weight;
+
+  if (pokemonHeight >= 6) {
+     document.write(
+      '<p>' + 
+      pokemonName +
+      ' ' +
+      '(Height: ' +
+       pokemonHeight +
+      ')' + 
+      " Wow, that's HUGE" + 
+      '</p>'
+      ); 
+    } 
+    else {
+    document.write( 
+        '<p>' +
+         pokemonName +
+          ' ' + 
+     '(Height: ' +
+      pokemonHeight +
+       ')' +
+       '</p>'
+       );
+    }
+}
