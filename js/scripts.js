776ff0bc3706a34487129e1bc1176c67622c00a9
FoxyHart <88792549+FoxyHart@users.noreply.github.com>
2021-11-03 13:15:09 -0400

    Polyfill and API
    Added Polyfill for Promises and Fetch. Also Linked to API

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,23 @@
 let pokemonRepository = (function() {
 //added IIFE function
-let pokemonList = [ 
-    { name: "Chespin", type: [ 'Grass' ], height : "1", weight: "20" },
-    { name: "Quilladin", type: ['Grass' ], height: "2", weight: "64" },
-    { name: "Chesnaught", type: ['Grass' , 'Fighting'], height: "5", weight: "199"},
-    { name: "Fennekin", type: ['Fire'], height: "1", weight: "20"},
-    { name: "Braixen", type: ['Fire'], height: "3", weight: "32"},
-    { name: "Delphox", type: ['Fire' , 'Psychic'], height: "5", weight: "86"},
-    { name: "Froakie", type: ['Water'], height: "1", weight: "15"},
-    { name: "Frogadier", type: ['Water'], height: "2", weight: "24"},
-    { name: "Greninja", type: ['Water' , 'Dark'], height: "6", weight: "88"},
-    { name: "Bunnelby", type: ['Normal'], height: "1", weight: "11"},
-    { name: "Diggersby", type: ['Normal' , 'Ground'], height: "3", weight: "93"},
-    { name: "Zigzagoon", type: ['Normal'], height: "1", weight: "39"},
-    { name: "Linoone", type: ['Normal'], height: "1", weight: "72"}
-];
-
-//for adding to pokedex entry { name: '', type: ['' , ''], height: , weight: }, or { name: '', type: [''], height: , weight: }, 
+let pokemonList = [];
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+// API for Pokemon and their stats
 function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (
+        typeof pokemon === "object" &&
+        "name" in pokemon
+      ) {
+        pokemonList.push(pokemon);
+      } else {
+        console.log("pokemon is not correct");
+      }
+    
 }
 function getAll() {
-    return pokemonList
+    return pokemonList;
 }
-// funtion to update 
+// function to update 
 function addListItem(pokemon) {
     let pokemonContainer = document.querySelector('.pokemon-list');
     let listPokemon = document.createElement('li');
@@ -32,24 +26,58 @@ function addListItem(pokemon) {
     button.classList.add('button-class');
     listPokemon.appendChild(button);
     pokemonContainer.appendChild(listPokemon);
-
-    button.addEventListener('Here', function() {
+    button.addEventListener("click", function() {
         showDetails(pokemon)
-    })
+    });
 }
-
-function showDetails(pokemon) {
-    console.log(pokemon.name)
+ function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+      item.weight = details.weight;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+        console.log(item);
+      });
   }
 
-return {
-    getAll: getAll,
+  return {
     add: add,
-    addListItem: addListItem
-};
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
 })();
 
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon)
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
