5355ce4602a0f785d20b1d72e8eb65fea2b1139e
FoxyHart <88792549+FoxyHart@users.noreply.github.com>
2021-11-05 17:55:40 -0400

    Update
    updating to include more comments, including Modal and more

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,69 @@
 let pokemonRepository = (function() {
 //added IIFE function
+
+let modalContainer = document.querySelector('#modal-container');
+//function for modal to pop up when a pokemon is selected
+function showModal(pokemon) {
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'X';
+    closeButtonElement.addEventListener('click', hideModal);
+
+// header for name
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
+//
+    let typeElement = document.createElement('p');
+    typeElement.innerText = pokemon.type;
+ 
+    let heightElement = document.createElement('p');
+    heightElement.innerText = 'Height: ' + pokemon.height;
+
+    let weightElement = document.createElelment('p');
+    weightElement.innerText = 'Weight: ' + pokemon.weight;
+
+    let imgElement = document.createElement('img')
+    imgElement.src = pokemon.imageUrl
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(typeElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(weightElement);
+    modal.appendChild(imgElement);
+
+    modalContainer.appendChild(modal);
+    modalContainer.classList.add('is-visible');
+}
+//function for modal removal
+function hideModal() {
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.classList.remove('is-visible');
+}
+//hide modal by ESC
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();
+  }
+});
+// hide modal when clicked away from 
+modalContainer.addEventListener('click', (e) => {
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
+//end of modal
 let pokemonList = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 // API for Pokemon and their stats
+
 function add(pokemon) {
-    if (
-        typeof pokemon === "object" &&
-        "name" in pokemon
-      ) {
         pokemonList.push(pokemon);
-      } else {
-        console.log("pokemon is not correct");
-      }
-    
 }
 function getAll() {
     return pokemonList;
@@ -26,7 +77,7 @@ function addListItem(pokemon) {
     button.classList.add('button-class');
     listPokemon.appendChild(button);
     pokemonContainer.appendChild(listPokemon);
-    button.addEventListener("click", function() {
+    button.addEventListener('click', function() {
         showDetails(pokemon)
     });
 }
@@ -61,7 +112,7 @@ function addListItem(pokemon) {
     });
   }
   function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function () {
+    loadDetails(item).then(function () {
         console.log(item);
       });
   }
